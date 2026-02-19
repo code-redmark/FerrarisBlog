12803a45a910e825fe5fa3ff269168012810b1b8
Marco <fallace.marco@gmail.com>
2026-02-19 15:51:27 +0100

    Prova con i prop, modificato il readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App(props) {
   return (
     <div>
-      <h1>Ferraris Blog</h1>
+      <h1>{props.nome}</h1>
     </div>
   )
 }

1ac52f90cdf10d127608f426f061eb5a38997364
Perchiante3 <raidshadowlegendssucksamogus@gmail.com>
2026-03-05 14:48:32 +0000

    WIP bottone funzionante con css ma font da aggiustare

--- src/componenti/base/Testa.jsx
+++ src/componenti/base/Testa.jsx
@@ -1,8 +1,6 @@
 import { Link } from 'react-router-dom'
-import logolight from "../Assets/logolight.png" // logo light mode
-import logodark from "../Assets/logodark.png" // logo dark mode
-import button from "../Assets/button.png" // questo è un template da usare eventualmente, non ha nessuna scritta sopra
-import buttonori from "../Assets/buttonorientamento.png"
+import logolight from "../Assets/Img/logolight.png" // logo light mode
+import logodark from "../Assets/Img/logodark.png" // logo dark mode
 import "../../stile/base/Testa.css"
 
 export default function Testa() {
@@ -15,8 +13,8 @@ export default function Testa() {
                     </Link>
                 </li>
                 <li>
-                    <Link to="/Orientamento">
-                        <img id='button' src={buttonori} alt='BUTTON' style={{ width: "160px", height: "auto"}}/>
+                    <Link to="/Orientamento" className='button-text'>
+                        Orientamento
                     </Link>
                 </li>
             </ul>

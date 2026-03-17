fb3de45e3a152f0b1788eb00011323c0264b8e74
Marco <fallace.marco@gmail.com>
2026-03-17 21:32:30 +0100

    WIP: non ho fatto quasi niente ma mi devo trovare la roba su codespaces, sto impostando il server e l'uso di API per fetch degli asset e della roba

---
+++ server/Classe.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+/**
+ * Lo schema è appunto uno "schema" che utilizziamo per qualunque dato all'interno del database
+ * lo schema dei post conterrà tutti i metadati di un determinato post
+ */
+
+const SchemaClasse = new mongoose.Schema({
+    anno: Number,
+    sezione: String,
+    bio: String,
+
+    
+    posts: [{
+        title: String,
+        description: String,
+        content: String,
+        dataPost: Date,
+         slug: String
+    }]
+
+});
+
+
+/**
+ * Mongoose.model crea una collection in MongoDB usando lo schema dato,
+ * una collection è un insieme di documenti, un documento solitamente è ad esempio
+ * un file json, come quello che conterrà tutte le classi, MongoDB nomina questa collection
+ * automaticamente con il plurale del documento, altrimenti, come ho fatto io, prende il terzo 
+ * argomento
+ */
+
+
+export default mongoose.model("Class", SchemaClasse, "classes")

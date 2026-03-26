d55126bf9c1dbc9618e11f0a4c786e5bcec72758
Sigma Demise <s.fioretti.s.20966@ferraris.org>
2026-03-26 19:10:44 +0000

    Modificato styling e testo del pulsante per la scrollbar da mobile

--- src/componenti/base/Testa.jsx
+++ src/componenti/base/Testa.jsx
@@ -22,9 +22,9 @@ export default function Testa() {
                         <p className='TestoHome'>Chi Siamo</p>
                     <div id='arcobaleno' className="arcobaleno_tasto_chi_siamo"></div>
                     </Link>
-                    <div className="open_scrollbar_div">
+                     <div className="open_scrollbar_div">
                     <button onClick={() => setIsOpen(!isOpen)} id="scrlbr_btn" className="open_scrollbar_button">
-                        <p>{isOpen ? "Chiudi" : "Apri"} scrollbar</p>
+                        <p className='TestoScrollbar'>{isOpen ? "Chiudi" : "Apri"} Lista Classi</p>
                     </button>
                     </div>
                 </li>

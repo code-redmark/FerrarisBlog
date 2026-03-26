be5c848d4fba4b3f64025841f26cce2c4d4571f8
Pape-6-9 <favaraemanuele77@gmail.com>
2026-03-26 13:54:02 +0000

    logo spostato ancora, barre bianche fixate destra sinistra sopra e sotto, margini aggiunti destra e sinistra messo testo al centro (da modificare non so che scrivere)

--- src/componenti/base/Piedi.jsx
+++ src/componenti/base/Piedi.jsx
@@ -2,17 +2,28 @@ import '../../stile/base/Piedi.css'
 
 export default function Piedi() {
     return (
-        
-        <div className="Piedi">
-       
-            <img id="logoPIEDI" src="/FerrarisBlog/assets/img/LOGOPIEDI.png" alt="logoPIEDI" />
-            <p>I NOSTRI CONTATTI</p>
-            <p>Email: paolone@FERRARIS.ORG </p>
-            <p>Telefono: +39 palle </p>
-        </div>
+        <>
+            <div className="Piedi">
+                <div className="logopiedi">
+                <img id="logoPIEDI" src="/FerrarisBlog/assets/img/LOGOPIEDI.png" alt="logoPIEDI" />
+                </div>
+                <div className= "contatti">
+                <p>I NOSTRI CONTATTI</p>
+                <p>Email: paolone@FERRARIS.ORG</p>
+                <p>Telefono: +39 palle</p> 
+                </div>  
+                  <div className="piedicentro">
+                <p>palle</p>
+                <p>palle2</p>
+                <p>instagram: massimo_bossetti</p> 
+            </div>
+            </div>
 
-        
+       
+        </>
     )
 }
+
+
 
 
